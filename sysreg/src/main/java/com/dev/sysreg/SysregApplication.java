9e70d10b53c1fd602a85626603a8660efbb0636f
Kouu777 <mart.vidalsarav06@gmail.com>
2026-04-22 22:01:03 -0400

    feat: Initial project folder structure

---
+++ sysreg/src/main/java/com/dev/sysreg/SysregApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sysreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysregApplication.class, args);
+	}
+
+}
---
+++ sysreg/src/test/java/com/dev/sysreg/SysregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sysreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
